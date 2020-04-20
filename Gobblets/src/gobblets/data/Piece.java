54e881f5cf5738e1cd1204a11683528aeb542172
Titouan lebocq <lebocq.tit@gmail.com>
2020-04-20 13:36:34 +0200

    création piece.java

--- Gobblets/src/gobblets/data/Piece.java
+++ Gobblets/src/gobblets/data/Piece.java
@@ -1,5 +1,21 @@
 package gobblets.data;
 
 public class Piece {
-    
+    private Couleur couleur;
+    private final Taille taille;
+    public Piece(Taille t) {
+        taille = t;
+    }
+
+    public Taille getTaille() {
+        return taille;
+    }
+
+    public Couleur getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(Couleur c) {
+        couleur = c;
+    }
 }
