bda4d83c806cd7311f4291bdda9fa28706047963
Titouan lebocq <lebocq.tit@gmail.com>
2020-04-21 17:45:37 +0200

    maj choix action JoueurHumain

--- Gobblets/src/gobblets/data/JoueurHumain.java
+++ Gobblets/src/gobblets/data/JoueurHumain.java
@@ -1,16 +1,46 @@
 package gobblets.data;
 
+import gobblets.IHM.IHM;
+import gobblets.IHM.texte.SaisieConsole;
 import gobblets.interaction.Action;
+import gobblets.interaction.Deplacement;
+import gobblets.interaction.Placement;
+import gobblets.interaction.Termination;
 
 public class JoueurHumain extends Joueur {
     public JoueurHumain(String nom, Couleur couleur) {
         super(nom, couleur);
-
     }
 
     @Override
     public Action choisirAction(Plateau p) {
-        // TODO Auto-generated method stub
-        return null;
+        IHM i = new SaisieConsole();
+        ActionType choix = i.saisirAction(this);
+        switch (choix) {
+            case PLACER: return creerActionPlacer(i, p);
+            case DEPLACER: return creerActionDeplacer(i, p);
+            case QUITTER: return creerActionQuitter(i, p);
+            default: return null;
+        }
+    }
+
+    private Action creerActionPlacer(IHM i, Plateau p) {
+        int[] coord = i.saisirCoordonnees();
+        Taille t = i.saisirTaille();
+        Case destination = p.getPlateau()[coord[0]][coord[1]];
+        return new Placement(t, destination);
+    }
+
+    private Action creerActionDeplacer(IHM i, Plateau p) {
+        int[] coord;
+        coord = i.saisirCoordonnees();
+        Case origin = p.getPlateau()[coord[0]][coord[1]];
+        coord = i.saisirCoordonnees();
+        Case destination = p.getPlateau()[coord[0]][coord[1]];
+        return new Deplacement(origin, destination);
+    }
+
+    private Action creerActionQuitter(IHM i, Plateau p) {
+        return new Termination();
     }
 }
